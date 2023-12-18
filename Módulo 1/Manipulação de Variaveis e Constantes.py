c6704bf13a09b45de2ced1d1806c91423d90a174
TiagoNelvo <ti.609@hotmail.com>
2023-12-18 19:14:22 -0300

    uso das variaveis no Python

--- Módulo 1/Manipulação de Variaveis e Constantes.py
+++ Módulo 1/Manipulação de Variaveis e Constantes.py
@@ -1,5 +1,39 @@
+#Variáveis Inteiras 
+#Int()
 numero = -3
 numero_jogos = 14
 numero_convidados = 15
 numero
 print(numero)
+print(numero,", ", numero_convidados)
+
+#Variáveis Float (ponto flutuante)
+#Float
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+print(pi)
+print(numero_euler)
+print(escala_terremoto)
+
+#Strings e Chars
+#Str
+letra = 'a'
+palavra1 = 'linguagem de programação'
+palavra2 = 'Python'
+print(letra, palavra1)
+print('Estou aprendendo uma', palavra1)
+print('Esta', palavra1, 'se chama', palavra2)
+
+#Exemplos
+#int(para transformar em números inteiros)
+idade = int(input('Digite sua idade:'))
+print('Sua idade é ', idade)
+
+#float (para transformar em números reais)
+pH = float(input('Qual o pH do solo durante a última medição?'))
+print('O pH medido foi ', pH)
+
+#str (para transformtar em texto)
+nome = str(input('Qual o seu nome?'))
+print('Seu nome é ', nome)
