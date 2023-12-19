ec63b1795be849ade3d466d85f6db8c97e396318
TiagoNelvo <ti.609@hotmail.com>
2023-12-19 17:21:54 -0300

    raiz quadrada

--- Módulo 1/Operações Matemáticas.py
+++ Módulo 1/Operações Matemáticas.py
@@ -20,3 +20,9 @@ print('O resto da Divisão é', 10 % 3)
 
 #potência
 print('5 elevado a 3 é', 5**3)
+
+#importar bibliotecas
+#math é a biblioteca de matematica
+import math
+math.sqrt(81)
+print('A raiz de 81 é', math.sqrt(81))
