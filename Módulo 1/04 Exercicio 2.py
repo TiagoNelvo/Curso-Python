729fc654ecfcfa63311444225af4aa603fec8a86
TiagoNelvo <ti.609@hotmail.com>
2023-12-19 18:14:47 -0300

    mudança de int para float nas variaveis

--- Módulo 1/04 Exercicio 2.py
+++ Módulo 1/04 Exercicio 2.py
@@ -1,8 +1,8 @@
-tempo = int(input('Quantas horas durou a viajem?'))
-velocidade = int(input('Qual a velocidade média durante a viagem?'))
+tempo = float(input('Quantas horas durou a viajem?'))
+velocidade = float(input('Qual a velocidade média durante a viagem?'))
 distancia = tempo * velocidade
 litros_usados = distancia / 12
 print('O tempo gasto foi de: ', tempo, 'horas')
 print('A velocidade foi de: ', velocidade, 'km/h')
 print('A distância foi de: ', distancia, 'kilometros')
-print('A quantidade de combustível gasta foi ', litros_usados, 'litros')
+print('A quantidade de combustível gasta foi ', round(litros_usados,1), 'litros')
