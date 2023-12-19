e074fc0ba9a9ac8f2240b20d95650ab2876b05fd
TiagoNelvo <ti.609@hotmail.com>
2023-12-19 17:26:09 -0300

    Arredondamento

--- Módulo 1/Operações Matemáticas.py
+++ Módulo 1/Operações Matemáticas.py
@@ -26,3 +26,11 @@ print('5 elevado a 3 é', 5**3)
 import math
 math.sqrt(81)
 print('A raiz de 81 é', math.sqrt(81))
+
+#Arredondamento
+casos_doenca = 134
+numero_habitantes = 34432
+casos_por_habitantes = casos_doenca / numero_habitantes
+print(casos_por_habitantes)
+round(casos_por_habitantes, 6)
+print('O número de casos por habitantes é de ', round(casos_por_habitantes, 4))
