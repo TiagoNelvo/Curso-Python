f06e50a4a125615d70f7a6aeda9a157e89cc5045
TiagoNelvo <ti.609@hotmail.com>
2023-12-19 17:13:55 -0300

    opeções básicas de matemática com variáveis

---
+++ Módulo 1/Operações Matemáticas.py
@@ -0,0 +1,16 @@
+a = 5
+b = 3
+print(a, b)
+
+#Soma
+a + b
+print('A soma é', a + b)
+
+#Subtração
+print('A subtração é', a - b)
+
+#Divisão
+print('A Divisão é', a / b)
+
+#Multiplicação
+print('A Multiplicação é', a * b)
