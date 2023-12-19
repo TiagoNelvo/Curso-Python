9ce5f27c35bfbd0821dd447bc7222f81b6337038
TiagoNelvo <ti.609@hotmail.com>
2023-12-19 17:48:16 -0300

    exercicio de python, operações basicas

--- Módulo 1/04 Exercicio 1.py
+++ Módulo 1/04 Exercicio 1.py
@@ -1,1 +1,7 @@
-print ("Hello Marginal")
+#1
+n1 = int(input('digite o primeiro número'))
+n2 = int(input('digite o segundo número'))
+print('O resultado da adição foi ', n1 + n2,) 
+print('A subtração foi', n1 - n2)
+print('A multiplicação foi', n1 * n2)
+print('A divisão foi', n1 / n2)
