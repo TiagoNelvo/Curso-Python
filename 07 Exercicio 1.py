d3ecca5d1d8d3deabadf6a5cf32569f65c270c2f
TiagoNelvo <ti.609@hotmail.com>
2023-12-27 19:37:14 -0300

    elif

--- 07 Exercicio 1.py
+++ 07 Exercicio 1.py
@@ -2,9 +2,7 @@ n = int(input('Digite a idade da criança'))
 
 if n >= 0 and n <= 12:
     print('Criança')
-else:
-    if n > 12 and n <= 17:
-        print('Adolescente')
-    else:
-        if n >= 18:
-            print('Adulto')
+elif n > 12 and n <= 17:
+    print('Adolescente')
+elif n >= 18:
+    print('Adulto')
---
+++ 07 Exercicio 2.py
@@ -0,0 +1,17 @@
+#Variaveis
+m1 = float(input('Digite a primeira nota: '))
+m2 = float(input('Digite a segunda nota: '))
+m3 = float(input('Digite a terceira nota: '))
+#Media
+media_notas = (m1 + m2 + m3)/3
+print(media_notas)
+#Código
+if media_notas >= 0.0 and media_notas <= 4.0:
+    print('Reprovado')
+else:
+    if media_notas >= 4.1 and media_notas <= 6.0:
+        
+    else:
+        if media_notas > 6:
+            print('aprovado')
+       
