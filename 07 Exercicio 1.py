0a9fdc09663b35cf9a5dbfe3fb82ccd03f24f119
TiagoNelvo <ti.609@hotmail.com>
2023-12-27 18:53:01 -0300

    Idade

--- 07 Exercicio 1.py
+++ 07 Exercicio 1.py
@@ -1,6 +1,10 @@
-n = input('Digite a idade da criança')
+n = int(input('Digite a idade da criança'))
+
 if n >= 0 and n <= 12:
     print('Criança')
 else:
     if n > 12 and n <= 17:
         print('Adolescente')
+    else:
+        if n >= 18:
+            print('Adulto')
