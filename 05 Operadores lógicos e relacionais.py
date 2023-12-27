1d96c21ca2a39b1527ed773c87696daf5e296393
TiagoNelvo <ti.609@hotmail.com>
2023-12-27 17:48:38 -0300

    Operações

--- 05 Operadores lógicos e relacionais.py
+++ 05 Operadores lógicos e relacionais.py
@@ -1,3 +1,13 @@
 a = True
 b = False
 print(a,b)
+
+a and b
+# as duas variaveis tem q ser verdadeiras para retortarnar True na condição "And"
+
+a & b
+
+# O "&" equivale ao "and"
+
+c = a and b 
+print('')
