c2aa7136320211fec4c452f34a8171c7f5aad2f8
TiagoNelvo <ti.609@hotmail.com>
2023-12-28 14:51:33 -0300

    while

--- 09 Estrutura de repetição (while).py
+++ 09 Estrutura de repetição (while).py
@@ -1,1 +1,19 @@
 # Estruturas de Repetição While
+
+numero = 1 
+while  numero < 6:
+    print (numero)
+    numero += 1
+print('___')
+
+numero = 5
+while numero > 0:
+    print(numero)
+    numero -= 1
+    
+soma = 0
+numero = 1 
+while numero < 6:
+    soma += numero
+    numero += 1
+print(soma)
