7a4a1f154929a54b41d3e2881cdf97ce6e4df41e
TiagoNelvo <ti.609@hotmail.com>
2023-12-28 13:56:07 -0300

    Exercicio 2

--- 07 Exercicio 2.py
+++ 07 Exercicio 2.py
@@ -1,14 +1,23 @@
 #Variaveis
+
 m1 = float(input('Digite a primeira nota: '))
 m2 = float(input('Digite a segunda nota: '))
 m3 = float(input('Digite a terceira nota: '))
+
 #Media
+
 media_notas = (m1 + m2 + m3)/3
 print(media_notas)
+
 #Código
+
 if media_notas >= 0.0 and media_notas <= 4.0:
     print('Reprovado')
 elif media_notas >= 4.1 and media_notas <= 6.0:
-    elif media_notas > 6:
-            print('aprovado')
-       
+    exame = float(input('Digite a nota do exame'))
+    if exame >= 6.0:
+        print('Aprovado')
+    else:
+        print('Reprovado')
+else:
+    print('Aprovado')
