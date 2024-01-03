cf16238097ea142720de9bc6ccd5c16908ae88ea
TiagoNelvo <ti.609@hotmail.com>
2024-01-03 10:35:20 -0300

    exercicio 2

---
+++ 10 exercicio 2.py
@@ -0,0 +1,14 @@
+#usando For
+for i in range(1, 11):
+    print('3 x {} = {}'.format(i, 3 * i))
+print('---')
+    
+#usando o while
+#variaveis
+numero = 1
+
+#codigo
+while numero <= 10:
+    print('3 x {} = {}'.format(numero, 3 * numero))
+    numero += 1
+print('---')
